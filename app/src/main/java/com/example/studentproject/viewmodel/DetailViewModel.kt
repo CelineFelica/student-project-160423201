70202c22dfb83d2a249d007240eb5df3c5d10da7
CelineFelica <167593460+CelineFelica@users.noreply.github.com>
2026-04-14 11:05:40 +0700

    Implement network fetching logic in `DetailViewModel`.
    - Update `fetch` method in `DetailViewModel` to accept a student ID and perform a GET request using Volley.
    - Implement Gson parsing within `DetailViewModel` to filter and update `studentLD` with the matching student record.
    - Update `StudentDetailFragment` to pass a student ID to the `viewmodel.fetch()` call.
    - Add `.idea/studiobot.xml` to the project configuration.

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.studentproject.viewmodel
 
 import android.app.Application
+import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import java.lang.reflect.Method
 
 class DetailViewModel(application: Application):
     AndroidViewModel(application) {
@@ -9,8 +12,22 @@ class DetailViewModel(application: Application):
     private var TAG = "volley_TAG"
     private var queue: RequestQueue?= null
 
-    fun fetch() {
+    fun fetch(id: String) {
         val url = "https://www.jsonkeeper.com/b/LLMN"
+
+        var stringRequest = StringRequest(Request, Method.GET, url,
+            {
+                val sType = object: TypeToken<List<Student>>(){}.type
+                val result = Gson().fromJson<List<Student>>(it, sType)
+                val students = result as ArrayList<Student>
+                studentLD.value = students.find{it.id == id}
+            },
+            {
+                Log.e("volley_status", it.message.toString())
+            })
+        StringRequest.tag = TAG
+        queue?.add(stringRequest)
+
         /*val student1 = Student("16055","Nonie","1998/03/28","5718444778",
             "http://dummyimage.com/75x100.jpg/cc0000/ffffff") */
         //studentLD.value = student1
