84f81b2b0f21ce9e4b7aa6e9bad45a3b383f0ea6
CelineFelica <167593460+CelineFelica@users.noreply.github.com>
2026-04-14 10:43:59 +0700

    Implement Volley network request setup in `DetailViewModel`.
    - Update `DetailViewModel` to inherit from `AndroidViewModel` to access application context.
    - Initialize `RequestQueue` and define a URL for student data retrieval.
    - Replace hardcoded student data with a placeholder for network fetching logic.

--- app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/studentproject/viewmodel/DetailViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.studentproject.viewmodel
 
-class DetailViewModel : ViewModel() {
+import android.app.Application
+import com.android.volley.RequestQueue
+
+class DetailViewModel(application: Application):
+    AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    private var TAG = "volley_TAG"
+    private var queue: RequestQueue?= null
 
     fun fetch() {
-        val student1 = Student("16055","Nonie","1998/03/28","5718444778",
-            "http://dummyimage.com/75x100.jpg/cc0000/ffffff")
-        studentLD.value = student1
+        val url = "https://www.jsonkeeper.com/b/LLMN"
+        /*val student1 = Student("16055","Nonie","1998/03/28","5718444778",
+            "http://dummyimage.com/75x100.jpg/cc0000/ffffff") */
+        //studentLD.value = student1
     }
 
 
